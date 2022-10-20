7b13e9498cc720838be3034ed0709b5f46ff17e9
0cmat <mattcon022@gmail.com>
2022-10-20 20:24:26 -0300

    Gerador de senhas version 1.0

---
+++ Gerador de Senhas/gerador_senhas.py
@@ -0,0 +1,17 @@
+import random
+
+print('Bem-vindo ao gerador de senhas!')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?@$%&^*(),.;/1234567890'
+
+number = int(input('Quantidade senhas para ser gerados::\n'))
+length = int(input('Digite o tamanho da senha a ser gerada:\n'))
+
+print('Senhas geradas:\n')
+contador = 0
+for senhas in range(number):
+    senhas = ' '
+    contador = contador + 1
+    for c in range(length):
+        senhas += random.choice(chars)
+    print('Exemplo de senha ', contador ,':',senhas,'\n')
