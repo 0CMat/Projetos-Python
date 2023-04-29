ffeaa818ba876584cf8d48b70f343cfc3b8e9c28
0cmat <mattcon022@gmail.com>
2023-04-29 09:01:56 -0300

    API - OpenWeatherMap Funcionando

--- Identificador de Clima/API-Clima.py
+++ Identificador de Clima/API-Clima.py
@@ -1,16 +1,23 @@
 import requests
 from pprint import pprint
 
-API_Key = "*"
+API_Key = "d4abbe913da9feb3d26ab9be4737e064"
 city = input('Digite a cidade em qual deseja saber o tempo:\n')
 
 base_URL = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city
 
 weather_data = requests.get(base_URL).json()
 
-# cidade = 
+# cidade =
+print("Cidade:\n", weather_data['name'])
 # temp =
-# temp_max = 
+temperatura = round(273.15 - weather_data['main']['temp'],2)
+print("Temperatura Atual:\n", f"{temperatura}ºC")
+# temp_max =
+temperatura_max = round(273.15 - weather_data['main']['temp_max'],2)
+print("Temperatura Máxima:\n",f"{temperatura_max}ºC")
 # temp_min =
+temperatura_min = round(273.15 - weather_data['main']['temp_min'],2)
+print("Temperatura Mínima:\n",f"{temperatura_min}ºC")
 
-pprint(weather_data)
+# pprint(weather_data)
